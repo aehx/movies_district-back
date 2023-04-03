163b3868a4cc5f46a7dc8604e26c7917307498e0
kevin <kevin.pernet73@gmail.com>
2023-04-03 16:22:10 +0200

    Cors rectification

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ exports.app = app;
 
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 app.use(
   cors({
     origin: ["http://localhost:3000"],

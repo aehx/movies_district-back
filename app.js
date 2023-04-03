a09538cc5d41be5e1697a460c43fcbcf76a7a958
kevin <kevin.pernet73@gmail.com>
2023-04-03 16:53:32 +0200

    Cors rectification

--- app.js
+++ app.js
@@ -18,11 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  })
-);
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
